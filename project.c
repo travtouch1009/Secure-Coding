832c56d6f37dcf5472e0dd280a128030bdbf81f3
travtouch1009 <42974585+travtouch1009@users.noreply.github.com>
2020-02-20 16:38:55 -0500

    Update project.c

--- project.c
+++ project.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define MAX_LINE_CHAR 1000
+#define MAX_LINE_LENGTH 1000
 #define MAX_LINE_COUNT 200
 
 void readFile(char *arg)
@@ -14,18 +15,27 @@ void readFile(char *arg)
 
         //FID, line storage, and line array
         FILE *fp;
-        char lineBuf[MAX_LINE_CHAR];
-        char *lines[MAX_LINE_COUNT];
+        char lineBuf[MAX_LINE_LENGTH];
+        char lines[MAX_LINE_COUNT][MAX_LINE_LENGTH];
         fp = fopen(buf, "r");
 	int i = 0;
-
+	int counter = 0;
+	
         //read the lines of the file to the max line count
         while(fgets(lineBuf, sizeof(lineBuf), fp) && i < MAX_LINE_COUNT)
         {
                 lineBuf[sizeof(lineBuf) - 1] = '\0';
-                lines[i] = lineBuf;
+                strcpy(lines[i], lineBuf);
                 i++;
+		counter++;
         }
+	
+	//generate random seed and number
+	srand(time(0));
+	int upper = counter - 1;
+	int lower = 0;
+	int randNum = (rand() % (upper - lower + 1)) + lower;
+	printf("Random line #%d  :  %s", randNum, lines[randNum];
 }
 
 int main(int argc, char **argv)
